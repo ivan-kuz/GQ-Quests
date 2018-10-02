4c596d224061409d41aaa6fa21921e52d68f9790
Ivanus Kuzminus <kuzminivan2004@gmail.com>
2018-10-02 15:58:11 +0100

    Update alpha_version.py
    Done the very first part

--- The Mystery of Aquae Sulis/alpha_version.py
+++ The Mystery of Aquae Sulis/alpha_version.py
@@ -1,1 +1,14 @@
-print("Not started yet")
+import sys
+import time
+#Inventory won't be availible in version 0.1 (the current version) 
+def start():
+  print("---------------------------------------------------------") 
+  i = set()#Inventory 
+  print("Welcome to my text quest, 'The Mystery of Aquae Sulis'! To check out more cool quests, please visit quests.aquaesulis.gq.")
+  print("At any point in the game press q to quit, r to restart and i to see your inventory")
+  time.sleep(2)
+  first_stage()
+def first_stage():
+  pass
+
+start()
